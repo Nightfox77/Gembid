d0ca6624226663e7aeed3ba8ef840f6fd68ad49e
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-10-08 20:48:40 +0200

    mobilemenu done

---
+++ js/effects/mobilemenu.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.querySelector("#mobileMenuOverlay");
+const burgerIcon = document.querySelector("#burgerMenuIcon");
+const closeIcon = document.querySelector("#closeIcon");
+export function showMobileMenu() {
+    burgerIcon.style.display = "none";
+    closeIcon.style.display = "block";
+    mobileMenu.style.transform = "translateX(0%)";
+}
+export function hideMobileMenu() {
+    burgerIcon.style.display = "block";
+    closeIcon.style.display = "none";
+    mobileMenu.style.transform = "translateX(-100%)";
+}
---
+++ js/main.js
@@ -0,0 +1,12 @@
+import { hideMobileMenu, showMobileMenu } from "./effects/mobilemenu.js";
+const body = document.querySelector("body");
+
+body.addEventListener("click", function (event) {
+    
+    if (event.target.id === "burgerMenuIcon") {
+      showMobileMenu();
+    }
+    if (event.target.id === "closeIcon") {
+        hideMobileMenu();
+    }
+});
