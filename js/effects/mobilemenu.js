8b46b50906d42e21ca6c42c669ef0c4b37a6d79c
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-10-14 17:56:56 +0200

    gallery and footer done

--- js/effects/mobilemenu.js
+++ js/effects/mobilemenu.js
@@ -4,11 +4,13 @@ const closeIcon = document.querySelector("#closeIcon");
 
 
 export function showMobileMenu() {
+    document.body.style.overflow = "hidden";
     burgerIcon.style.display = "none";
     closeIcon.style.display = "block";
     mobileMenu.style.transform = "translateX(0%)";
 }
 export function hideMobileMenu() {
+    document.body.style.overflow = "auto";
     burgerIcon.style.display = "block";
     closeIcon.style.display = "none";
     mobileMenu.style.transform = "translateX(100%)";
