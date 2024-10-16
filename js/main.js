f20d405a7e2bb4530e70d3ba578e43b0f18c8a7d
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-10-16 14:28:32 +0200

    updated footer and updated responsivness

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { hideMobileMenu, showMobileMenu } from "./effects/mobilemenu.js";
-
+import { initIconToggle, removeIconToggle } from "./effects/footerMenu.js";
 const body = document.querySelector("body");
 
 body.addEventListener("click", function (event) {
@@ -14,3 +14,25 @@ body.addEventListener("click", function (event) {
     }
 });
 
+
+function handleResize() {
+  const width = window.innerWidth; // Get the current window width
+
+  if (width < 1100) {
+    // Hide all sublists when in mobile mode
+    const sublists = document.querySelectorAll('.listItems');
+    sublists.forEach(sublist => {
+        sublist.style.display = 'none'; // Hide all sublists
+    });
+    initIconToggle(); // Initialize icons for mobile view
+
+  } else {
+      removeIconToggle(); // Cleanup for desktop view
+  }
+}
+
+// Add event listener for window resize
+window.addEventListener('resize', handleResize);
+
+// Initial check when the page loads
+handleResize();
