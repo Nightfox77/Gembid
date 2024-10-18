dff9fd10a7335ddb2f799cf4d05bc2f5199be751
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-10-18 17:07:34 +0200

    changed mobileMenuModal from modal tag to dialog tag

--- js/effects/mobilemenu.js
+++ js/effects/mobilemenu.js
@@ -3,12 +3,12 @@ const burgerIcon = document.querySelector("#burgerMenuIcon");
 const closeIcon = document.querySelector("#closeIcon");
 
 
+
 function showMobileMenu() {
+    
     document.body.style.overflow = "hidden";
     burgerIcon.style.display = "none";
     closeIcon.style.display = "block";
-
-    
 }
 function hideMobileMenu() {
     document.body.style.overflow = "auto";
@@ -17,27 +17,29 @@ function hideMobileMenu() {
     
 }
 export function initModalEvents() {
-    const modal = document.getElementById('mobileMenuOverlay');
     
+    const modal = document.getElementById('mobileMenuOverlay');
     const openButton = document.getElementById('burgerMenuIcon');
     const closeButton = document.getElementById('closeIcon');
 
     // Show modal when button is clicked
     openButton.addEventListener('click', function () {
-      modal.classList.add('show');
-     
+      modal.showModal();
+      modal.classList.add("show");
       showMobileMenu();
     });
 
     // Close modal when close button is clicked
     closeButton.addEventListener('click', function () {
-      modal.classList.remove('show');
+      modal.close();
+      modal.classList.remove("show");
       hideMobileMenu();
     });
 
     // Close modal when clicking outside the modal content
     window.addEventListener('click', function (event) {
         if (event.target === modal) {
+          modal.close();
           modal.classList.remove('show');
           hideMobileMenu();
         }
